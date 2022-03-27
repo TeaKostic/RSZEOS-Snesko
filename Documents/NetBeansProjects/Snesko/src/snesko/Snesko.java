881499949ce2b9451fbc47bd632a795a8f186a3a
'teodora <'kosticteo@gmail.com'>
2022-03-27 23:42:33 +0200

    Konacna verzija

--- Documents/NetBeansProjects/Snesko/src/snesko/Snesko.java
+++ Documents/NetBeansProjects/Snesko/src/snesko/Snesko.java
@@ -1,17 +1,11 @@
 package snesko;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
@@ -56,7 +50,8 @@ public class Snesko {
                 System.out.println("2. Dodaj reč");
                 System.out.println("0. Izlaz");
                 System.out.println("Odaberi opciju: ");
-                String opcija = in.readLine();
+                               
+                String opcija = in.readLine(); // is used to read one line text at a time
                 
                 switch (opcija) {
                     case "1":
@@ -89,7 +84,7 @@ public class Snesko {
 
             System.out.println("Unesite rec koju zelite da dodate: ");
             BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            String novaRec = in.readLine(); // citanje sa ulaza
+            String novaRec = in.readLine(); 
 
             boolean postoji = false;
             for(String r : listaReci) {
@@ -137,16 +132,16 @@ public class Snesko {
     
     void novaIgra() {
         
-        List<String> listaReci = ucitajReciUListu();
+        List<String> listaReci = ucitajReciUListu(); 
         
         if (listaReci == null) {
             return;
         }
 
-        int ind = slucajanBroj(listaReci.size());
+        int ind = slucajanBroj(listaReci.size()); // used to get the number of elements in list
 
 //            System.out.println(listaReci.size());
-        System.out.println(listaReci.get(ind));
+        System.out.println(listaReci.get(ind)); // used to return the element at a given index from array/list
 
         // Rec dobijena nasumicno
         String odabranaRec = listaReci.get(ind);
@@ -154,38 +149,58 @@ public class Snesko {
         try {
             
             List<Character> unetaSlova = new ArrayList<Character>(); // prazna lista, sad sledi punjenje
-                    
+            int counter = 0;
+            
              while(true){
                  
                 prikaziRecZaPogadjanje(odabranaRec, unetaSlova);//znamo da je svakako 1 slovo taj string
-                System.out.println("Unesite slovo koje zelite da dodate: ");
+                System.out.println("Unesite slovo za pogadjanje ili konacno resenje: ");
                 BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-                String novoSlovo = in.readLine();
+                String unos = in.readLine();
                 
 
-                if(novoSlovo.matches("[АБВГДЂЕЖЗИЈКЛЉМНЊОПРСТЋУФХЦЧЏШабвгдђежзијклљмнњопрстћуфхцчџш]")) {
-                    if(!unetaSlova.contains(novoSlovo.charAt(0))) {    
-                        unetaSlova.add(novoSlovo.charAt(0));
-                        if(odabranaRec.contains(novoSlovo)){
-                           if (krajIgrePobedaProvera(odabranaRec, unetaSlova)){
+                if(unos.matches("[АБВГДЂЕЖЗИЈКЛЉМНЊОПРСТЋУФХЦЧЏШабвгдђежзијклљмнњопрстћуфхцчџш]+")) {
+                    unos = unos.toLowerCase();
+                    if(unos.length() == 1){
+                        if(!unetaSlova.contains(unos.charAt(0))) {    
+                            unetaSlova.add(unos.charAt(0));
+                            if(odabranaRec.contains(unos)){
+                               if (krajIgrePobedaProvera(odabranaRec, unetaSlova)){
+                                System.out.println("Igra je gotova! Pogodili ste trazenu rec: \"" + odabranaRec + "\"");
+                                break;
+                                } 
+                            } else {
+                                counter ++;
+                                System.out.println("Brojac iznosi " + counter);
+                                
+                                // int brPogrSl = brojPogresnihSlova(odabranaRec, unetaSlova);
+                                crtajSneska(counter);
+                                
+                                if (counter == 7) {
+                                    System.out.println("Izgubili ste, igra je zavrsena!");
+                                    break;
+                                }
+                            }
+
+                        } else {
+                            System.out.println("Slovo je vec uneto!");
+                        }
+                    } else {
+                        if(unos.equals(odabranaRec)){
                             System.out.println("Igra je gotova! Pogodili ste trazenu rec: \"" + odabranaRec + "\"");
                             break;
-                            } 
                         } else {
-                            int brPogrSl = brojPogresnihSlova(odabranaRec, unetaSlova);
-                            crtajSneska(brPogrSl);
-                            if (brPogrSl == 7) {
-                                System.out.println("Izgubili ste igra je zavrsena!");
-                                break;
-                            }
+                            //int brPogrSl = brojPogresnihSlova(odabranaRec, unetaSlova);
+                            counter++; 
+                            crtajSneska(counter);
                         }
-                        
-                    } else {
-                        System.out.println("Slovo je vec uneto");
                     }
+
+                } else {
+                    System.out.println("Pogresan unos! Dozvoljena su samo cirilicna slova!");
                 }
-                
             }
+                    
         } catch (Exception e) {
             System.out.println("greska prilikom upisa dodatog slova");
         }
@@ -198,7 +213,7 @@ public class Snesko {
         for (int i = 0; i < duzinaOdabraneReci; i++) {
            if(unetaSlova.contains(odabranaRec.charAt(i))) { // dobijam koje je slovo na tom indeksu, npr charAt(0)=r, charAt(1)=e.. reka
                sb.append(odabranaRec.charAt(i)); //append je nadogradnja stringa, dodajemo crticu ili slovu kroz iteraciju
-               continue; //нова итерација
+               continue; //нова итерација, dodavanje kroz svaku iteraciju crtice
            } 
            sb.append("_ ");
         }
@@ -217,19 +232,9 @@ public class Snesko {
         return true;
     }  
     
-    private int brojPogresnihSlova(String odabranaRec, List<Character> unetaSlova){
-        int brojPogresnihSlova = 0;
-        for (Character c : unetaSlova) { // iteriram kroz sva uneta slova i za svako provera dal je u odabranu rec
-            if(odabranaRec.indexOf(c) == -1){ // znaci da ne postoji
-                brojPogresnihSlova++;
-            }
-        }
-        return brojPogresnihSlova;
-    }
-    
-    private void crtajSneska (int brojPogresnihSlova) {
+    private void crtajSneska (int counter) {
        
-         switch(brojPogresnihSlova) {
+         switch(counter) {
              case 1 :  System.out.print("\n  (`^'^'`)\n" +
                                          "  `======'\n");
                        break;
